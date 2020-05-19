a2b0c54bc931171188ac3c7b107338cc31c27bf5
usefulparade <breadclip.cooperative@gmail.com>
2020-05-19 19:07:18 -0400

    added web midi version!

--- js/content.js
+++ js/content.js
@@ -9,6 +9,8 @@ var getContent = document.getElementById("getContent");
 var scoreButton = document.getElementById("scoreButton");
 var scoreContent = document.getElementById("scoreContent");
 
+var titleButton = document.getElementById("titleButton");
+
 
 aboutButton.addEventListener("click", function(event){
     event.preventDefault()
@@ -21,6 +23,10 @@ aboutButton.addEventListener("click", function(event){
 	aboutContent.style.maxHeight = "600px";
 	getContent.style.maxHeight = "0px";
 	scoreContent.style.maxHeight = "0px";
+
+	aboutButton.firstElementChild.setAttribute ("id", "active");
+	getButton.firstElementChild.setAttribute ("id", "");
+	scoreButton.firstElementChild.setAttribute ("id", "");
 });
 
 getButton.addEventListener("click", function(event){
@@ -34,6 +40,27 @@ getButton.addEventListener("click", function(event){
 	aboutContent.style.maxHeight = "0px";
 	getContent.style.maxHeight = "600px";
 	scoreContent.style.maxHeight = "0px";
+
+	aboutButton.firstElementChild.setAttribute ("id", "");
+	getButton.firstElementChild.setAttribute ("id", "active");
+	scoreButton.firstElementChild.setAttribute ("id", "");
+});
+
+titleButton.addEventListener("click", function(event){
+    event.preventDefault()
+    console.log('click!');
+
+    aboutContent.style.left = "-200%";
+	getContent.style.left = "0";
+	scoreContent.style.left = "200%";
+
+	aboutContent.style.maxHeight = "0px";
+	getContent.style.maxHeight = "600px";
+	scoreContent.style.maxHeight = "0px";
+
+	aboutButton.firstElementChild.setAttribute ("id", "");
+	getButton.firstElementChild.setAttribute ("id", "active");
+	scoreButton.firstElementChild.setAttribute ("id", "");
 });
 
 scoreButton.addEventListener("click", function(event){
@@ -47,4 +74,8 @@ scoreButton.addEventListener("click", function(event){
 	scoreContent.style.maxHeight = "600px";
 	aboutContent.style.maxHeight = "0px";
 	getContent.style.maxHeight = "0px";
+
+	aboutButton.firstElementChild.setAttribute ("id", "");
+	getButton.firstElementChild.setAttribute ("id", "");
+	scoreButton.firstElementChild.setAttribute ("id", "active");
 });
